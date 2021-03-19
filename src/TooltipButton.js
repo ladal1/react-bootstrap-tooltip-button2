9684c1319f39bb0b6d14261922e77d6bf5ab0c42
Ladislav Louka <loukalad@fit.cvut.cz>
2021-03-19 19:57:54 +0100

    React function to component

--- src/TooltipButton.js
+++ src/TooltipButton.js
@@ -1,21 +1,31 @@
 import React from 'react'
-import Button from 'react-bootstrap/lib/Button'
-import Tooltip from 'react-bootstrap/lib/Tooltip'
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger'
+import {Button, Tooltip, OverlayTrigger} from 'react-bootstrap'
 import './TooltipButton.css'
 
 // button that, if disabled, shows a tooltip explaining why it's disabled
 // by default, this component will render the button and disable it if prop disabled is set
 // but optionally, a rendered button can be passed as prop renderedButton (the caller must then disable it themselves)
 
-export default function({disabled = false, title = '', tooltipText, tooltipId, tooltipPlacement='bottom', renderedButton, ...props}) {
-	if (!disabled) return renderedButton? renderedButton : (<Button {...props}>{title}</Button>);
-	return (
-    <OverlayTrigger placement={tooltipPlacement} delayHide={150}
-      overlay={<Tooltip id={tooltipId}>{tooltipText}</Tooltip>}>
-      <div className="tooltip-button-helper">
-        {renderedButton? renderedButton : (<Button {...props} disabled>{title}</Button>)}
-      </div>
-    </OverlayTrigger>
-  );
+class TooltipButton extends React.Component{
+    static defaultProps = {
+        disabled: false,
+        title: '',
+        tooltipText: '',
+        tooltipId: 'tt1',
+        tooltipPlacement:'bottom',
+        renderedButton: null
+    }
+
+    render(){
+        return (
+            <OverlayTrigger placement={this.props.tooltipPlacement} delayHide={150}
+                            overlay={<Tooltip id={this.props.tooltipId}>{this.props.tooltipText}</Tooltip>}>
+                <div className="tooltip-button-helper">
+                    {this.props.renderedButton? this.props.renderedButton : (<Button {...this.props} disabled>{this.props.title}</Button>)}
+                </div>
+            </OverlayTrigger>
+        );
+    }
 }
+
+export default TooltipButton;
